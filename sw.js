fd5863941d0003f1b149e1fff59aff05a0061968
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2024-06-12 16:48:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,12 +8,12 @@ var APP_PREFIX = 'kstore_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_000001';
+var VERSION = 'version_000002';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
-  `${GHPATH}/hw`,
-  `${GHPATH}/walterwork`
+  `${GHPATH}/apps`,
+  `${GHPATH}/about`
 ]
